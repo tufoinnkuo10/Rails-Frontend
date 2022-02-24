5e087a506287af5898e6ba3bcac87c8220f4676a
tufoinnkuo10 <tufoinnkuo10@gmail.com>
2022-02-24 12:34:07 +0100

    add df local host to backend

--- src/redux/API.js
+++ src/redux/API.js
@@ -1,11 +1,10 @@
 import { loadMessageData } from './message';
 
-const mainURL = 'http://localhost:3000/messages';
+const mainURL = 'http://127.0.0.1:5000/messages';
 
 const getMessageData = async (dispatch) => {
   const response = await fetch(mainURL);
   const data = await response.json();
-  console.log('hello');
   dispatch(loadMessageData(data));
 };
 
